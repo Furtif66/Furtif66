b5438c08f7c0594ea138d495a04136c9aba7fde4
Furtif66 <80543688+Furtif66@users.noreply.github.com>
2021-04-08 08:28:00 +0200

    Add files via upload

--- mot_magique.py
+++ mot_magique.py
@@ -4,48 +4,71 @@ from tkinter import messagebox
 from tkinter import ttk
 import random
 
+min = 0
+max = 50
+y = 0
+nbre_magique = random.randint(min, max)
+
+couleurs=("aliceblue","aqua","bisque","blueviolet","chartreuse","coral","darkkhaki","darkorange","deepskyblue","gainsboro","indigo","khaki","lightpink","lightsteelblue","magenta","maroon","olive","salmon","peru")
+
 mainapp = tkinter.Tk()
 mainapp.title("mot magique")
-mainapp.geometry("1500x1200")
+mainapp.geometry("1500x950")
 mainapp.minsize(480, 360)
 mainapp.config(background='sandybrown')
 
-min = 0
-max = 50
-nbre_magique = random.randint(min, max)
-
-def calcul_nbre():
-    while not nbre_magique=="":
-        try:
-            reponse=input ("Trouves le nombre magique (entre 0 et 50) ")
-            if int(reponse)==nbre_magique:
-                print("BRAVO, tu as trouvé le nombre magique")
-                return
-            elif int(reponse)<nbre_magique:
-                print("Ton nombre est trop petit")
-            else :
-                print("Ton nombre est trop grand")
-        except :
-            print("Il faut noter un nombre (pas de lettres")
-
-
 def start_jeu():
+
+    def calcul():
+        global y
+
+        try:
+            valeur_nbre = int(entree.get())
+        except:
+            x = random.randint(0, 18)
+            reponse3 = Label(fen_jeu, text="Il faut noter un nombre (et pas de lettres) ", width=40, height=3, bg=couleurs[x], font=("Courrier", 20));
+            reponse3.place(x=230, y=400)
+            return
+
+        x = random.randint(0, 18)
+        y += 1
+
+        if valeur_nbre < nbre_magique:
+            reponse1 = Label(fen_jeu, text="Ton nombre est trop petit ", width=40, height=3, bg=couleurs[x], font=("Courrier", 20));
+            reponse1.place(x=230, y=400)
+
+        elif valeur_nbre > nbre_magique:
+            reponse2 = Label(fen_jeu, text="Ton nombre est trop grand ", width=40, height=3, bg=couleurs[x], font=("Courrier", 20));
+            reponse2.place(x=230, y=400)
+
+        else:
+            reponse = Label(fen_jeu, text=f"BRAVO, tu as trouvé le nombre magique {nbre_magique} en {y} essais", width=45, height=8, bg=couleurs[x], font=("Courrier", 30));
+            reponse.place(x=95, y=180)
+            y = 0
+            # bouton recommencer
+            recommencer = tkinter.Button(fen_jeu, text="RECOMMENCER", width=20, height=3, font=("Courrier", 12), bg='#FFCC99', command=start_jeu)
+            recommencer.place(x=500, y=450)
+
+    fen_jeu = tkinter.Toplevel()
+    fen_jeu.title("chercher nombre")
+    fen_jeu.geometry("1200x600")
+    fen_jeu.config(background='cadetblue')
+
     bouton_start_jeu.destroy()
-    fenetre_calcul=tkinter.Toplevel()
-    fenetre_calcul.title("mot magique")
-    fenetre_calcul.geometry("1500x200")
-    fenetre_calcul.config(background='blue')
+    nbre_magique = random.randint(min, max)
 
-    label_texte = tkinter.Label (fenetre_calcul, text="Trouves le nombre magique (entre 0 et 50) : ",width=50, height=5,bg='deeppink', font=("Courrier", 12));
-    label_texte.place(x=1, y=10)
+    label_texte = Label(fen_jeu, text="Trouve le nombre magique (entre 0 et 50) : ", width=50, height=5, bg='cadetblue', font=("Courrier", 20));
+    label_texte.place(x=200, y=50)
 
-    label_entree=tkinter.Entry(fenetre_calcul,font=("Courrier", 12), width=10);
-    label_entree.place(x=240, y=160)
-    
-    #calcul_nbre()
+    # entrée nombre
+    entree = Entry(fen_jeu, font=("Courrier", 20), width=3);
+    entree.place(x=550, y=180)
 
+    # bouton entrer nombre
+    b = Button(fen_jeu, text="ENTRER", width=10, height=2, font=("Courrier", 20), bg='#FFCC99', command=calcul)
+    b.place(x=490, y=260)
 
-# bouton start
+#bouton start
 bouton_start_jeu = tkinter.Button(mainapp, text="Démarrer le jeu", width=20, height=5, font=("Courrier", 40), bg='#339900',command=start_jeu);bouton_start_jeu.place(x=450, y=100)
 
 # bouton quitter
@@ -53,15 +76,6 @@ def fermer_fenetre():
     mainapp.destroy()
 quitter = tkinter.Button(mainapp, text="QUITTER", width=12, height=3, font=("Courrier", 12), bg='#FFCC99',command=fermer_fenetre)
 quitter.place(x=1350, y=850)
-
-
-
-
-
-
-
-
-
 
 
 
